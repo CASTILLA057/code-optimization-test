5e5987f3dd0a419427a4b3999d8ec22ae91f0480
Santiago Castilla <ScastillaC.05@gmail.com>
2024-09-14 16:16:41 -0500

    Refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -2,17 +2,13 @@ public class Store {
     public static void main(String[] args) {
         //Definimos la cantidad de ventas que se deben hacer minimo en el local
         final int LIMITE_VENTAS = 50;
-        //Definimos las contastntes que serian el precio de los productos 1,2 y 3
-        final int PRICE_PRODUCT_1 = 15;
-        final int PRICE_PRODUCT_2 = 10;
-        final int PRICE_PRODUCT_3 = 5;
-     
-    //Matriz con precio (en la fila 0) y cantidades de ventas (fila 1)
+    //Matriz con precio (en la fila 0) y cantidades de ventas (fila 1) ,utilizamos multidimensional
     int [][] PRODUCTS = {
-        {PRICE_PRODUCT_1, PRICE_PRODUCT_2, PRICE_PRODUCT_3},
+        {15,10,5},
         {2,3,4}
     };
     //Calculando  el total de ventas
+    int VENTAS_TOTALES = 0;
     for (int i = 0; i < PRODUCTS[0].length; i++) {
         VENTAS_TOTALES += PRODUCTS[0][i] * PRODUCTS[1][i];
     }  
